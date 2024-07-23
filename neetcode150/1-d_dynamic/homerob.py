107e69c342297707513f886b44048039fd223686
J ATULYA <109218299+jatulya@users.noreply.github.com>
2024-07-23 21:26:58 +0530

    Update homerob.py

--- neetcode150/1-d_dynamic/homerob.py
+++ neetcode150/1-d_dynamic/homerob.py
@@ -10,3 +10,20 @@ class Solution:
         
         return robnext
         
+''' Cpp solution where values are checked from starting of the array
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size()==1)
+            return nums[0];
+        
+        int prev2=0, curr=0, prev1=0;
+        for (int n: nums){
+            curr = max(prev2+n, prev1);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return curr;
+    }
+}; '''
